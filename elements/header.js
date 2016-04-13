696be4e5316519c08f41476b51f41bca8f68def9
sethvincent <sethvincent@gmail.com>
2016-04-12 18:41:56 -0700

    header styles, add npm scripts, bump seattle-boundaries to 4.1.2

--- elements/header.js
+++ elements/header.js
@@ -4,11 +4,20 @@ var css = require('sheetify')
 module.exports = function header (state, send) {  
   var prefix = css`
     :host {
-      background-color: #fafafa;
+      width: 100%;
+      background-color: #f2f2f0;
+      padding: 10px 0px;
+      height: 50px;
+      position: fixed;
+      top: 0;
+      border-bottom: 1px solid #e2e2e0;
     }
 
     h1 {
-      
+      font-size: 20px;
+      line-height: 1;
+      margin: 5px 0px 0px 0px;
+      display: inline-block;
     }
   `
 
