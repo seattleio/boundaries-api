f7c65f97ff8289887be6bfc88b77671837b3e1f2
sethvincent <sethvincent@gmail.com>
2016-04-15 15:02:16 -0700

    routing with sheet-router, initial global styles

--- browser.js
+++ browser.js
@@ -1,16 +1,31 @@
+var url = require('url')
 var el = require('yo-yo')
 var css = require('sheetify')
+var history = require('sheet-router/history')
+var sheetRouter = require('sheet-router')
+var href = require('sheet-router/href')
+var xtend = require('xtend')
 
 var send = require('send-action')({
   onaction: onaction,
   onchange: onchange,
   state: {
-    title: 'Seattle Boundaries'
+    title: 'Seattle Boundaries',
+    location: document.location.pathname
   }
 })
 
+css('./style.css', { global: true })
+
+function sendLocation (href) {
+  send('location', { location: url.parse(href).pathname })
+}
+
+href(sendLocation)
+history(sendLocation)
+
 /*
-* Require the main elements the app
+* Require the main elements of the app
 */
 var header = require('./elements/header')
 
@@ -18,6 +33,12 @@ var header = require('./elements/header')
 * action handler that modifies state based on the actions triggered
 */
 function onaction (action, state) {
+  var type = action.type
+
+  if (type === 'location') {
+    return xtend(state, { location: action.location })
+  }
+
   return state
 }
 
@@ -25,6 +46,8 @@ function onaction (action, state) {
 * Subscribe to changes to the store for rendering & logging
 */
 function onchange (action, state, oldState) {
+  console.log(action.type, action, state)
+
   el.update(document.getElementById('app'), render(state))
 }
 
