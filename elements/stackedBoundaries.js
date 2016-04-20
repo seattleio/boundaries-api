31b475795b6ad56bf1d0c74bf65e5429a0366f68
Adrian Limon <alimon808@gmail.com>
2016-04-19 17:00:53 -0700

    improved styling for stackedBoundaries component

--- elements/stackedBoundaries.js
+++ elements/stackedBoundaries.js
@@ -26,18 +26,20 @@ module.exports = function stackedBoundaries (state, send) {
     margin:0;
     border:0;
     height: 15px;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
   }
   `
   return el`<div class="${prefix}">
     <img src='icon-person-128.png' />
     <div style='margin-top:-10px;'>
-    <div width='15px'><hr style='margin-right:42.5%; margin-left:42.5%; background:black;'></div>
-    <div width='25px'><hr style='margin-right:40%; margin-left:40%; background:green;'></div>
-    <div width='30px'><hr style='margin-right:35%; margin-left:35%; background:red;'></div>
-    <div width='50px'><hr style='margin-right:25%; margin-left:25%; background:blue;'></div>
-    <div width='60px'><hr style='margin-right:20%; margin-left:20%; background:purple;'></div>
-    <div width='80px'><hr style='margin-right:10%; margin-left:10%; background:yellow;'></div>
-    <div width='100px'><hr style='margin-right:0%; margin-left:0%; background:brown;'></div>
+    <div width='15px'><hr style='margin: 0 42.5%; background:black;'></div>
+    <div width='25px'><hr style='margin: 0 40%; background:green;'></div>
+    <div width='30px'><hr style='margin: 0 35%; background:red;'></div>
+    <div width='50px'><hr style='margin: 0 25%; background:blue;'></div>
+    <div width='60px'><hr style='margin: 0 20%; background:purple;'></div>
+    <div width='80px'><hr style='margin: 0 10%; margin-left:10%; background:yellow;'></div>
+    <div width='100px'><hr style='margin: 0 0; margin-left:0%; background:brown;'></div>
     </div>
     </div>
   </div>`
